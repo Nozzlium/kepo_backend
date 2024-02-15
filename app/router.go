7f3dbfba04943ae2c95c71cdc5b69fe1b0d1e0c6
Nozzlium <Cesa263@gmail.com>
2024-02-15 12:17:39 +0700

    removed CORS to try matching URL with nginx

--- app/router.go
+++ app/router.go
@@ -106,17 +106,6 @@ func NewRouter() *httprouter.Router {
 
 	router.GET("/api/category", categoryController.Get)
 
-	router.GlobalOPTIONS = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get("Access-Control-Request-Method") != "" {
-			header := w.Header()
-			header.Set("Access-Control-Allow-Methods", header.Get("Allow"))
-			header.Set("Access-Control-Allow-Origin", "*")
-			header.Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		}
-
-		w.WriteHeader(http.StatusNoContent)
-	})
-
 	return router
 
 }
--- helper/write_response.go
+++ helper/write_response.go
@@ -9,10 +9,6 @@ func WriteResponse(
 	writer http.ResponseWriter,
 	response interface{},
 ) {
-	writer.Header().Add("Content-Type", "application/json")
-	writer.Header().Set("Access-Control-Allow-Origin", "*")
-	writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTION")
-	writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 	encoder := json.NewEncoder(writer)
 	err := encoder.Encode(response)
 	PanicIfError(err)
